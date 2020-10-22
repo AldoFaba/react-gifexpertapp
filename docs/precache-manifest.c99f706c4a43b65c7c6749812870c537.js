31363a4b20341bd4b16b7c89479f834fbd345727
aldo.faba <aldo.faba@elektra.com.mx>
2020-10-22 17:39:39 -0500

    docs agregado

---
+++ docs/precache-manifest.c99f706c4a43b65c7c6749812870c537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "269c02ed3a19e1794fb11315fa3efef7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31909550e5f635a1b197",
+    "url": "/static/css/main.b5bf19f8.chunk.css"
+  },
+  {
+    "revision": "e915424eeb4a1675bf19",
+    "url": "/static/js/2.7e8e1ec7.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.7e8e1ec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31909550e5f635a1b197",
+    "url": "/static/js/main.610a5f64.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
